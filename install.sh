aaf6d515465728ea221ee430d855a1f2a00a7d3a
Eric <305237@qq.com>
2020-10-19 14:30:11 +0800

    更新安装器

--- install.sh
+++ install.sh
@@ -18,26 +18,26 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # Prepare output
-echo -e "|\n|   EHEH Installer\n|   ===================\n|"
+echo -e "|\n|   EHEH 一键安装\n|   ===================\n|"
 
 # Root required
 if [ $(id -u) != "0" ];
 then
-	echo -e "|   Error: You need to be root to install the EHEH agent\n|"
-	echo -e "|          The agent itself will NOT be running as root but instead under its own non-privileged user\n|"
+	echo -e "|   Error: 你需要用root账号执行该脚本\n|"
+	echo -e "|          安装脚本将创建一个EHEH账号用来定时执行脚本\n|"
 	exit 1
 fi
 
 # Parameters required
 if [ $# -lt 1 ]
 then
-	echo -e "|   Usage: bash $0 'token'\n|"
+	echo -e "|   用法: bash $0 'token'\n|"
 	exit 1
 fi
 
 
 #delete exits data
-echo "|" && read -p "|   It will delete old local data if exist ,Do you want to continue? 安装将删除本地的以前记录  [Y/n] " input_del
+echo "|" && read -p "|   安装将删除本地的以前记录  [Y/n] " input_del
 if [ -z $input_del ] || [ $input_del == "Y" ] || [ $input_del == "y" ]
 then
   #uninstall
@@ -54,29 +54,29 @@ if [ ! -n "$(command -v crontab)" ]
 then
 
 	# Confirm crontab installation
-	echo "|" && read -p "|   Crontab is required and could not be found. Do you want to install it?  [Y/n] " input_variable_install
+	echo "|" && read -p "|   当前系统没安装Crontab，无法定时推送数据，安装吗？?  [Y/n] " input_variable_install
 
 	# Attempt to install crontab
 	if [ -z $input_variable_install ] || [ $input_variable_install == "Y" ] || [ $input_variable_install == "y" ]
 	then
 		if [ -n "$(command -v apt-get)" ]
 		then
-			echo -e "|\n|   Notice: Installing required package 'cron' via 'apt-get'"
+			# echo -e "|\n|   Notice: Installing required package 'cron' via 'apt-get'"
 		    apt-get -y update
 		    apt-get -y install cron
 		elif [ -n "$(command -v yum)" ]
 		then
-			echo -e "|\n|   Notice: Installing required package 'cronie' via 'yum'"
+			# echo -e "|\n|   Notice: Installing required package 'cronie' via 'yum'"
 		    yum -y install cronie
 
 		    if [ ! -n "$(command -v crontab)" ]
 		    then
-		    	echo -e "|\n|   Notice: Installing required package 'vixie-cron' via 'yum'"
+		    	# echo -e "|\n|   Notice: Installing required package 'vixie-cron' via 'yum'"
 		    	yum -y install vixie-cron
 		    fi
 		elif [ -n "$(command -v pacman)" ]
 		then
-			echo -e "|\n|   Notice: Installing required package 'cronie' via 'pacman'"
+			# echo -e "|\n|   Notice: Installing required package 'cronie' via 'pacman'"
 		    pacman -S --noconfirm cronie
 		fi
 	fi
@@ -94,23 +94,23 @@ if [ -z "$(ps -Al | grep cron | grep -v grep)" ]
 then
 
 	# Confirm cron service
-	echo "|" && read -p "|   Cron is available but not running. Do you want to start it? [Y/n] " input_variable_service
+	echo "|" && read -p "|   系统定时任务Crontab没启动，要启动吗? [Y/n] " input_variable_service
 
 	# Attempt to start cron
 	if [ -z $input_variable_service ] || [ $input_variable_service == "Y" ] || [ $input_variable_service == "y" ]
 	then
 		if [ -n "$(command -v apt-get)" ]
 		then
-			echo -e "|\n|   Notice: Starting 'cron' via 'service'"
+			# echo -e "|\n|   Notice: Starting 'cron' via 'service'"
 			service cron start
 		elif [ -n "$(command -v yum)" ]
 		then
-			echo -e "|\n|   Notice: Starting 'crond' via 'service'"
+			# echo -e "|\n|   Notice: Starting 'crond' via 'service'"
 			chkconfig crond on
 			service crond start
 		elif [ -n "$(command -v pacman)" ]
 		then
-			echo -e "|\n|   Notice: Starting 'cronie' via 'systemctl'"
+			# echo -e "|\n|   Notice: Starting 'cronie' via 'systemctl'"
 		    systemctl start cronie
 		    systemctl enable cronie
 		fi
@@ -120,7 +120,7 @@ then
 	if [ -z "$(ps -Al | grep cron | grep -v grep)" ]
 	then
 		# Show error
-		echo -e "|\n|   Error: Cron is available but could not be started\n|"
+		echo -e "|\n|   Error: Crontab定时任务没启动\n|"
 		exit 1
 	fi
 fi
@@ -144,7 +144,7 @@ fi
 mkdir -p /etc/EHEH
 
 # Download agent
-echo -e "|   Downloading EHEH-agent.sh to /etc/EHEH\n|\n|   + $(wget -nv -o /dev/stdout -O /etc/EHEH/eheh-agent.sh --no-check-certificate https://raw.github.com/eheh-org/agent/master/eheh-agent.sh)"
+echo -e "|   下载脚本到 /etc/EHEH\n|\n|   + $(wget -nv -o /dev/stdout -O /etc/EHEH/eheh-agent.sh --no-check-certificate https://raw.github.com/eheh-org/agent/master/eheh-agent.sh)"
 
 if [ -f /etc/EHEH/eheh-agent.sh ]
 then
@@ -164,29 +164,30 @@ then
 	crontab -u EHEH -l 2>/dev/null | { cat; echo "*/3 * * * * bash /etc/EHEH/eheh-agent.sh > /etc/EHEH/eheh-cron.log 2>&1"; } | crontab -u EHEH -
 
 	# Show success
-	echo -e "|\n|   Success: The EHEH agent has been installed\n|"
+	echo -e "|\n|   小提示：如果你的服务器安装有宝塔，crontab可能被停用了，请参考：https://eheh.org/article/10 进行设置下"
+	echo -e "|\n|   小提示：如果你的服务使用了Cpanel，ping可能缺少权限，请参考https://eheh.org/article/6 进行设置下"
 	echo -e "|   Success: 安装完成\n|"
 
-  echo "|" && read -p "|   Do you want to monitoring MYSQL state? enter y if you want。你想监控mysql状态吗，如果要请输入Y，我们将帮你设置  [Y/n] " input_mysql
+  echo "|" && read -p "|   你想监控mysql状态吗，如果要请输入Y，我们将帮你设置  [Y/n] " input_mysql
   if [ -z $input_mysql ] || [ $input_mysql == "Y" ] || [ $input_mysql == "y" ]
   then
-        echo "|" && read -p "|   Enter your mysql username。请输入mysql用户名  " input_mu
+        echo "|" && read -p "|  请输入mysql用户名  " input_mu
         $(echo "$input_mu" > /etc/EHEH/mysql-user)
 
-        echo "|" && read -p "|   Enter your mysql password。 请输入mysql密码  " input_mp
+        echo "|" && read -p "|  请输入mysql密码  " input_mp
         $(echo "$input_mp" > /etc/EHEH/mysql-pass)
 
         echo -e "|\n|   Done,完成设置"
   fi
 
   #push data at once ?
-  echo "|" && read -p "|   Do you want to push data at once，or please wait for 5 minutes? 需要马上推送数据吗？或等5/6分钟  [Y/n] " push
-  if [ -z $push ] || [ $push == "Y" ] || [ $push == "y" ]
+  echo "|" && read -p "|   需要马上推送数据吗？或等5/6分钟  [Y/n] " push
+  if [ $push == "Y" ] || [ $push == "y" ]
   then
     sh /etc/EHEH/eheh-agent.sh
   fi
 
-  echo -e "|\n|   Done: Enjoy your EHEH！完成安装！EHEH.ORG 上见\n|"
+  echo -e "|\n| 完成安装！EHEH.ORG 上见\n|"
 
 	# Attempt to delete installation script
 	if [ -f $0 ]
@@ -195,5 +196,5 @@ then
 	fi
 else
 	# Show error
-	echo -e "|\n|   Error: The EHEH agent could not be installed\n|"
+	echo -e "|\n|  错误，无法安装\n|"
 fi
