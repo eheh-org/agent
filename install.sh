26739fb1d27a1455fe6fd8faa6c9f92abafa929f
Eric <305237@qq.com>
2020-10-24 09:23:13 +0800

    修正debian 缺省shell导致的推送问题，增加netstat、ping的检测和安装

--- install.sh
+++ install.sh
@@ -17,6 +17,15 @@
 # Set environment
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+_yellow() {
+    printf '\033[0;31;33m%b\033[0m' "$1"
+}
+_blue() {
+    printf '\033[0;31;36m%b\033[0m' "$1"
+}
+
+clear
+
 # Prepare output
 echo -e "|\n|   EHEH 一键安装\n|   ===================\n|"
 
@@ -42,8 +51,8 @@ if [ -z $input_del ] || [ $input_del == "Y" ] || [ $input_del == "y" ]
 then
   #uninstall
   rm -Rf /etc/EHEH
-  crontab -u EHEH -r
-  userdel EHEH
+  crontab -u EHEH -r 1>/dev/null 2>/dev/null
+  userdel EHEH     1>/dev/null 2>/dev/null
 else
   exit 1
 fi
@@ -85,6 +94,74 @@ then
 	then
 	    # Show error
 	    echo -e "|\n|   Error: Crontab is required and could not be installed\n|"
+	    exit 1
+	fi
+fi
+
+
+# Check if netstat is installed
+if [ ! -n "$(command -v netstat)" ]
+then
+
+	echo "|" && read -p "|   当前系统没安装netstat，安装吗？?  [Y/n] " input_variable_install
+
+	# Attempt to install crontab
+	if [ -z $input_variable_install ] || [ $input_variable_install == "Y" ] || [ $input_variable_install == "y" ]
+	then
+		if [ -n "$(command -v apt-get)" ]
+		then
+		    apt-get -y update
+		    apt-get -y install net-tools
+		elif [ -n "$(command -v yum)" ]
+		then
+		    yum -y install net-tools
+		elif [ -n "$(command -v pacman)" ]
+		then
+		    pacman -S --noconfirm netstat-nat
+		fi
+	fi
+
+	if [ ! -n "$(command -v netstat)" ]
+	then
+	    # Show error
+	    echo -e "|\n|   错误：自动安装netstat过程中出现错误,一般是你取消了安装或源或者依赖问题，请处理后再进行\n|"
+	    echo -e "|\n|        如果你系统是Ubuntu 20.04, 19.10 或者 19.04，且提示错误是libpng12-0缺失，可参考以下解决\n|"
+	    echo -e "|        add-apt-repository ppa:linuxuprising/libpng12|"
+	    echo -e "|        apt update|"
+	    echo -e "|        apt install libpng12-0\n|"
+	    echo -e "|\n|        如果你系统是其他版本的ubuntu，或其他操作系统，请自行搜索解决\n|"
+	    printf  "$(_blue "|   netstat命令没安装好，将无法统计MYSQL基本指标、Web端口信息，如您不需要统计这2项，可不做处理。") \n\n"
+#	    exit 1
+	fi
+fi
+
+
+# Check if netstat is installed
+if [ ! -n "$(command -v ping)" ]
+then
+
+	echo "|" && read -p "|   当前系统没安装ping，安装吗？?  [Y/n] " input_variable_install
+
+	# Attempt to install crontab
+	if [ -z $input_variable_install ] || [ $input_variable_install == "Y" ] || [ $input_variable_install == "y" ]
+	then
+		if [ -n "$(command -v apt-get)" ]
+		then
+		    apt-get -y update
+		    apt-get -y install iputils-ping
+		elif [ -n "$(command -v yum)" ]
+		then
+		    yum -y install iputils
+		elif [ -n "$(command -v pacman)" ]
+		then
+		    pacman -S --noconfirm iputils
+		fi
+	fi
+
+	if [ ! -n "$(command -v ping)" ]
+	then
+	    # Show error
+	     echo -e "|\n|   错误：自动安装ping过程中出现错误,一般是源或者依赖问题，请根据提示修复后再进行\n|"
 	    exit 1
 	fi
 fi
@@ -143,7 +220,10 @@ fi
 # Create agent dir
 mkdir -p /etc/EHEH
 
-# Download agent
+# Download agent -offical website
+#echo -e "|   下载脚本到 /etc/EHEH\n|\n|    + $(wget -nv -o /dev/stdout -O /etc/EHEH/eheh-agent.sh --no-check-certificate https://eheh.org/shell/eheh-agent.sh)"
+
+#github link
 echo -e "|   下载脚本到 /etc/EHEH\n|\n|   + $(wget -nv -o /dev/stdout -O /etc/EHEH/eheh-agent.sh --no-check-certificate https://raw.github.com/eheh-org/agent/master/eheh-agent.sh)"
 
 if [ -f /etc/EHEH/eheh-agent.sh ]
@@ -166,7 +246,8 @@ then
 	# Show success
 	echo -e "|\n|   小提示：如果你的服务器安装有宝塔，crontab可能被停用了，请参考：https://eheh.org/article/10 进行设置下"
 	echo -e "|\n|   小提示：如果你的服务使用了Cpanel，ping可能缺少权限，请参考https://eheh.org/article/6 进行设置下"
-	echo -e "|   Success: 安装完成\n|"
+	echo -e "|\n|"
+	printf  "$(_blue "|   安装完成，开始设置") \n\n"
 
   echo "|" && read -p "|   你想监控mysql状态吗，如果要请输入Y，我们将帮你设置  [Y/n] " input_mysql
   if [ -z $input_mysql ] || [ $input_mysql == "Y" ] || [ $input_mysql == "y" ]
@@ -180,13 +261,23 @@ then
         echo -e "|\n|   Done,完成设置"
   fi
 
-  #push data at once ?
-  echo "|" && read -p "|   需要马上推送数据吗？或等5/6分钟  [Y/n] " push
-  if [ $push == "Y" ] || [ $push == "y" ]
-  then
-    sh /etc/EHEH/eheh-agent.sh
-    chown -Rf EHEH.EHEH /etc/EHEH/
-  fi
+
+ if [ -n "$(command -v apt-get)" ]
+ then
+    printf  "$(_yellow "|   Debian/ubuntu的默认shell已经切换成了dash。如需马上推送一次数据，请手动执行以下命令")\n\n"
+    printf  "$(_blue "|   /etc/EHEH/eheh-agent.sh && chown -Rf EHEH.EHEH /etc/EHEH/") \n\n"
+ else
+      #push data at once ?
+      echo "|" && read -p "|   需要马上推送数据吗？或等5/6分钟  [Y/n] " push
+      if [ $push == "Y" ] || [ $push == "y" ]
+      then
+        sh /etc/EHEH/eheh-agent.sh
+        chown -Rf EHEH.EHEH /etc/EHEH/
+      fi
+ fi
+
+
+
 
   echo -e "|\n| 完成安装！EHEH.ORG 上见\n|"
 
